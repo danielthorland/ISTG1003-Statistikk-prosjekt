e7b4d84672c762a9bc1b68b7c22da646fcf64d60
Sondre Sætervadet <sondsae@stud.ntnu.no>
2024-11-08 14:40:57 +0100

    finished repetisjonsfaktor

--- prosjekt/Oppgave1/repetisjonsFaktor.py
+++ prosjekt/Oppgave1/repetisjonsFaktor.py
@@ -10,8 +10,10 @@ df['Repetisjonsfaktor'] = df['Unique_Pieces'] / df['Pieces']
 # Lag et horisontalt histogram for å bytte om på x- og y-aksen
 plt.figure(figsize=(10, 6))
 plt.hist(df['Repetisjonsfaktor'], bins=20, color='skyblue', edgecolor='black', alpha=0.7)
-plt.title('Distribusjon av Repetisjonsfaktor for LEGO-sett (Horisontalt)')
-plt.ylabel('Repetisjonsfaktor')
-plt.xlabel('Antall LEGO-sett')
+plt.title('Forklaringsvariabel 1: Repetisjonsfaktor')
+plt.ylabel('Antall LEGO-sett')
+plt.xlabel('Repetisjonsfaktor')
 plt.grid(axis='x', linestyle='--', alpha=0.5)
+plt.savefig("Repetisjonsfaktor.pdf", format="pdf")
 plt.show()
+
